67288b68a09a5693a3d493f3f9fc51647093c34c
Francisco1asir <fparnav1908@g.educaand.es>
2024-02-26 19:02:31 +0100

    push1

---
+++ src/model/productos/dataproductos.ts
@@ -0,0 +1,9 @@
+export async function getProductos() {
+
+    try{
+        const res = await fetch('http://213.136.94.185:3001/api/libros/listar', {cache: 'no-store'})
+        return res.json()
+    }catch(error){
+            throw new Error('Failed to fetch data')
+    }
+}
