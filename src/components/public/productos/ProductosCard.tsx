305cd39f28a1ab936fff1cafbfaa0a600eddddbc
Trino10 <trinotijola10@gmail.com>
2024-03-10 18:11:56 +0100

    tarjeta-motos

--- src/components/public/productos/ProductosCard.tsx
+++ src/components/public/productos/ProductosCard.tsx
@@ -1,28 +1,33 @@
 import { IProducto } from '@/interfaces/IProductos';
-import { Card, CardBody, CardFooter, CardHeader, Image } from '@nextui-org/react';
+import { Button, Card, CardBody, CardFooter, CardHeader, Image } from '@nextui-org/react';
 import React, { FC } from 'react'
 
 interface Props {
     productos: IProducto;
 }
 
-export const ProductosCard:FC<Props> = ({productos}) => {
-  return (
-    <section className='sct'>
-    <Card className="py-4 min-h-72 max-h-72">
-        <CardHeader className="pb-0 pt-2 px-4 h-10 flex-col items-start">
-            <h5 className="font-bold text-sm">{ productos.marca }</h5>
-        </CardHeader>
-        <CardBody className="overflow-visible h-48 py-2 flex flex-wrap justify-center items-center">
-            <Image
-                alt="Card background"
-                className="object-cover rounded-xl max-h-44"
-                src={productos.foto}
-            />
-        </CardBody>
-        <CardFooter className="pb-0 pt-2 px-4 h-10 flex-col items-start">
-        </CardFooter>
-    </Card>
-    </section>
-  )
+export const ProductosCard: FC<Props> = ({ productos }) => {
+    return (
+        <section className='sct'>
+            <Card className="py-4 min-h-72 max-h-80 bg-[url(https://www.publicdomainpictures.net/pictures/40000/nahled/abstract-green-background-1361187771Euk.jpg)]">
+                <CardHeader className="pb-0 pt-2 px-4 h-10 flex-col items-start">
+                    <h2 className="font-bold text-xl text-white">{productos.marca}</h2>
+                    <h6 className="font-bold text-sm text-white">{productos.modelo}</h6>
+                </CardHeader>
+                <CardBody className="overflow-visible h-48 py-2 flex flex-wrap justify-center items-center">
+                    <Image
+                        alt="Card background"
+                        className="object-cover rounded-xl max-h-44 transition-transform ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-700"
+                        src={productos.foto}
+                    />
+                </CardBody>
+                <CardFooter className="pb-0 pt-2 px-4 h-10 flex-col items-center">
+                    <Button className="size-25 text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+                        Ver Detalle
+                    </Button>
+
+                </CardFooter>
+            </Card>
+        </section>
+    )
 }
